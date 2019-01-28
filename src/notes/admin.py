fbdec99db5fb8ad4de1c8674967cc22049266fbe
Miroslav Sivon <miroslav.sivon@teradata.com>
2019-01-28 07:21:03 +0100

    try to change admin page

--- src/notes/admin.py
+++ src/notes/admin.py
@@ -3,16 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Entry, Business, BusinessCategories, BusinessTypes
 
-admin.site.register(Entry)
-admin.site.register(Business)
-admin.site.register(BusinessTypes)
+# admin.site.register(Entry)
+# admin.site.register(Business)
+# admin.site.register(BusinessTypes)
 
 class InLineBusinessType(admin.TabularInline):
     model = BusinessTypes
 
-class BusinessCatergoryAdmin(admin.ModelAdmin):
-    inlines = [InLineBusinessType]
-    list_display = ('business_category',)
+class InLineBusinessCategory(admin.TabularInline):
+    model = BusinessCategories
+
+class BusinessAdmin(admin.ModelAdmin):
+    inlines = [InLineBusinessType, InLineBusinessCategory]
+    # list_display = ('business_category',)
     # fields = ('business_category')
 
-admin.site.register(BusinessCategories, BusinessCatergoryAdmin)
+admin.site.register(Business, BusinessAdmin)
